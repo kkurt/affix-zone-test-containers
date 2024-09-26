cfcc0a6b5af0154bf7a5e6f7c8f420e09af00d39
kkurt <kursat@kursatkurt.com>
2024-09-26 17:43:16 +0300

    son

---
+++ helm-charts/delete.bat
@@ -0,0 +1,2 @@
+helm uninstall affixzone-test-containers --namespace affixzone-test-containers
+kubectl delete namespace affixzone-test-containers
--- helm-charts/redeploy.bat
+++ helm-charts/redeploy.bat
@@ -1,3 +1,4 @@
 helm uninstall affixzone-test-containers --namespace affixzone-test-containers
+kubectl delete namespace affixzone-test-containers
 helm package test-containers
 helm install affixzone-test-containers affixzone-test-containers-0.1.0.tgz --create-namespace --namespace affixzone-test-containers
